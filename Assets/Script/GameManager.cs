882f9cb861313af1abdb9da1f185432f50e6f99a
kimilljoo <71426788+kimilljoo@users.noreply.github.com>
2022-02-22 18:09:09 +0900

    02.22.2022

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,35 +5,24 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    private List<GameObject> infomationList = new List<GameObject>();
+    public string playTime { get; private set; }
+
+    private float timeSec;
+    private float timeMin;
 
     private void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-            UpdateInformation();
+        timeCalc();
+        playTime = timeMin.ToString("00") + ":" + timeSec.ToString("00");
     }
-
-    private void UpdateInformation() // 역시 하드코딩.
+    private void timeCalc()
     {
-        MainCharacterScript playerinfomation = PlayerInfomation.Instance.player.GetComponent<MainCharacterScript>();
-
-        List<float> list = playerinfomation.playerInfomation.ReturnPlayerInfo();
-
-        UpdateInformationText(list);
-    }
-
-    private void UpdateInformationText(List<float> list) // 하드코딩. 만약 좋은 방법 있으면 그걸로
-    {
-        if (list.Count != 15) return;
-
-        for (int i = 0; i < list.Count; i++) 
+        timeSec += Time.deltaTime;
+        if(timeSec >= 59.5f)
         {
-            if (i == 0 || i == 2 || i == 8 || i == 14)
-                infomationList[i].GetComponent<Text>().text = list[i].ToString();
-            else
-                infomationList[i].GetComponent<Text>().text = ((list[i]* 100)-100).ToString() + "%";
+            timeSec = 0f;
+            timeMin += 1f;
         }
     }
+
 }
