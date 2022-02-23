47bf7f9c18e176cab5815caec28b63810826476b
BottleCap20 <tjrkdgnl20@gmail.com>
2022-02-23 12:02:54 +0900

    2022-02-23 (1)
    ●버그 해결
    ●아이템의 자석범위 수정 스크립트 생성
    
    ●할 일
    - 아이템의 드랍 포인트 생성
    - 체력 아이템
    - etc...

--- Assets/Script/Item/Item.cs
+++ Assets/Script/Item/Item.cs
@@ -4,16 +4,47 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    [SerializeField] protected GameObject target;
-    [SerializeField] private float followSpeed = 5.0f;
-    public bool check { private get; set; }
-    protected bool durationItem { private get; set; }
+    [SerializeField] protected GameObject target;       // 타겟 (플레이어)
+
+    [SerializeField] private float followSpeed = 5.0f;  // 타겟을 따라가는 속도
+
+  
+    [HideInInspector] public float detectRadius;        // 자석 범위
+    public bool check { private get; set; }             // 반대로 움직일지 체크
+    
+    protected bool durationItem { private get; set; }   // 지속시간이 있는지 체크
+
+    private CircleCollider2D triggerCollider;           // 자석 범위 콜라이더
 
     private void Start()
     {
         target = GameObject.Find("Player");
+        ChangeRadius(target.GetComponent<MainCharacterScript>().playerInfomation.magnet);
     }
-    private IEnumerator FollowTarget()
+
+    public void ChangeAllRadius(float Radius) // 현재 나와있는 모든 아이템의 자석범위를 바꾼다. 매개변수로 플레이어의 자석범위를 넣기
+    {
+        foreach (var curItem in GameObject.FindObjectsOfType<Item>())
+        {
+            curItem.ChangeRadius(Radius);
+        }
+    }
+
+    private void ChangeRadius(float Radius) // 현재 아이템의 자석범위를 바꾼다.
+    {
+        foreach(var curTrigger in GetComponents<CircleCollider2D>())
+        {
+            if (curTrigger.isTrigger == true)
+                triggerCollider = curTrigger;
+        }
+        if (triggerCollider != null)
+        {
+            detectRadius = Radius;
+            triggerCollider.radius = detectRadius;
+        }
+    }
+
+    private IEnumerator FollowTarget() // 타겟을 따라간다.
     {
         while (true)
         {
@@ -25,7 +56,8 @@ public class Item : MonoBehaviour
     {
         StartCoroutine(FollowTarget());
     }
-    private IEnumerator MoveConverseTarget()
+
+    private IEnumerator MoveConverseTarget() // 타겟의 반대로 움직인다.
     {
         check = true;
         Vector3 converseTarget = new Vector2((this.transform.position.x - target.transform.position.x), (this.transform.position.y - target.transform.position.y)).normalized;
@@ -41,8 +73,7 @@ public class Item : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log($"{collision.gameObject}{target}");
-        if (collision == !target)
+        if (collision.gameObject != target)
         {
             return;
         }
@@ -52,7 +83,7 @@ public class Item : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject == !target && collision.collider == target.GetComponent<Collider2D>())
+        if (collision.gameObject != target && collision.collider != target.GetComponent<Collider2D>())
             return;
 
         IItemWork thisItemWork = this.GetComponent<IItemWork>();
@@ -74,7 +105,7 @@ public class Item : MonoBehaviour
     }
 }
 
-interface IItemWork
+interface IItemWork // 아이템의 효과처리 인터페이스
 {
     void ItemWork();
 }
